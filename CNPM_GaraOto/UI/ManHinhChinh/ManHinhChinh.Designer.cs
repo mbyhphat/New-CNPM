93cc2a8103ede18abaa4a604a2f0e0fe2a67e0f8
mbyhphat <22521067@gm.uit.edu.vn>
2024-06-05 21:42:55 +0700

    sửa lỗi hiển thị

--- CNPM_GaraOto/UI/ManHinhChinh/ManHinhChinh.Designer.cs
+++ CNPM_GaraOto/UI/ManHinhChinh/ManHinhChinh.Designer.cs
@@ -71,11 +71,10 @@
             tableLayoutPanel1.Controls.Add(BaoCaoPhieuNhap_Button, 8, 0);
             tableLayoutPanel1.Dock = DockStyle.Top;
             tableLayoutPanel1.Location = new Point(0, 0);
-            tableLayoutPanel1.Margin = new Padding(7, 7, 7, 7);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Size = new Size(2430, 146);
+            tableLayoutPanel1.Size = new Size(1012, 62);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // TienCong_Button
@@ -83,10 +82,9 @@
             TienCong_Button.BackColor = SystemColors.ActiveCaptionText;
             TienCong_Button.Dock = DockStyle.Fill;
             TienCong_Button.ForeColor = SystemColors.ButtonHighlight;
-            TienCong_Button.Location = new Point(279, 7);
-            TienCong_Button.Margin = new Padding(7, 7, 7, 7);
+            TienCong_Button.Location = new Point(116, 3);
             TienCong_Button.Name = "TienCong_Button";
-            TienCong_Button.Size = new Size(255, 132);
+            TienCong_Button.Size = new Size(106, 56);
             TienCong_Button.TabIndex = 7;
             TienCong_Button.Text = "Nội dung sửa chữa";
             TienCong_Button.UseVisualStyleBackColor = false;
@@ -97,10 +95,9 @@
             BaoCaoTonKho_Button.BackColor = SystemColors.ActiveCaptionText;
             BaoCaoTonKho_Button.Dock = DockStyle.Fill;
             BaoCaoTonKho_Button.ForeColor = SystemColors.ButtonHighlight;
-            BaoCaoTonKho_Button.Location = new Point(1355, 7);
-            BaoCaoTonKho_Button.Margin = new Padding(7, 7, 7, 7);
+            BaoCaoTonKho_Button.Location = new Point(564, 3);
             BaoCaoTonKho_Button.Name = "BaoCaoTonKho_Button";
-            BaoCaoTonKho_Button.Size = new Size(255, 132);
+            BaoCaoTonKho_Button.Size = new Size(106, 56);
             BaoCaoTonKho_Button.TabIndex = 4;
             BaoCaoTonKho_Button.Text = "Lập báo cáo tồn";
             BaoCaoTonKho_Button.UseVisualStyleBackColor = false;
@@ -111,10 +108,9 @@
             VatTuPhuTung_Button.BackColor = SystemColors.ActiveCaptionText;
             VatTuPhuTung_Button.Dock = DockStyle.Fill;
             VatTuPhuTung_Button.ForeColor = SystemColors.ButtonHighlight;
-            VatTuPhuTung_Button.Location = new Point(1086, 7);
-            VatTuPhuTung_Button.Margin = new Padding(7, 7, 7, 7);
+            VatTuPhuTung_Button.Location = new Point(452, 3);
             VatTuPhuTung_Button.Name = "VatTuPhuTung_Button";
-            VatTuPhuTung_Button.Size = new Size(255, 132);
+            VatTuPhuTung_Button.Size = new Size(106, 56);
             VatTuPhuTung_Button.TabIndex = 3;
             VatTuPhuTung_Button.Text = "Vật tư phụ tùng";
             VatTuPhuTung_Button.UseVisualStyleBackColor = false;
@@ -125,10 +121,9 @@
             LapPhieuSuaChua_Button.BackColor = SystemColors.ActiveCaptionText;
             LapPhieuSuaChua_Button.Dock = DockStyle.Fill;
             LapPhieuSuaChua_Button.ForeColor = SystemColors.ButtonHighlight;
-            LapPhieuSuaChua_Button.Location = new Point(817, 7);
-            LapPhieuSuaChua_Button.Margin = new Padding(7, 7, 7, 7);
+            LapPhieuSuaChua_Button.Location = new Point(340, 3);
             LapPhieuSuaChua_Button.Name = "LapPhieuSuaChua_Button";
-            LapPhieuSuaChua_Button.Size = new Size(255, 132);
+            LapPhieuSuaChua_Button.Size = new Size(106, 56);
             LapPhieuSuaChua_Button.TabIndex = 2;
             LapPhieuSuaChua_Button.Text = "Lập phiếu sửa chữa";
             LapPhieuSuaChua_Button.UseVisualStyleBackColor = false;
@@ -139,10 +134,9 @@
             TraCuuXe_Button.BackColor = SystemColors.ActiveCaptionText;
             TraCuuXe_Button.Dock = DockStyle.Fill;
             TraCuuXe_Button.ForeColor = SystemColors.ButtonHighlight;
-            TraCuuXe_Button.Location = new Point(548, 7);
-            TraCuuXe_Button.Margin = new Padding(7, 7, 7, 7);
+            TraCuuXe_Button.Location = new Point(228, 3);
             TraCuuXe_Button.Name = "TraCuuXe_Button";
-            TraCuuXe_Button.Size = new Size(255, 132);
+            TraCuuXe_Button.Size = new Size(106, 56);
             TraCuuXe_Button.TabIndex = 1;
             TraCuuXe_Button.Text = "Tra cứu xe";
             TraCuuXe_Button.UseVisualStyleBackColor = false;
@@ -153,10 +147,9 @@
             TiepNhanXe_Button.BackColor = SystemColors.ActiveCaptionText;
             TiepNhanXe_Button.Dock = DockStyle.Fill;
             TiepNhanXe_Button.ForeColor = SystemColors.ButtonHighlight;
-            TiepNhanXe_Button.Location = new Point(7, 7);
-            TiepNhanXe_Button.Margin = new Padding(7, 7, 7, 7);
+            TiepNhanXe_Button.Location = new Point(3, 3);
             TiepNhanXe_Button.Name = "TiepNhanXe_Button";
-            TiepNhanXe_Button.Size = new Size(258, 132);
+            TiepNhanXe_Button.Size = new Size(107, 56);
             TiepNhanXe_Button.TabIndex = 0;
             TiepNhanXe_Button.Text = "Tiếp nhận xe";
             TiepNhanXe_Button.UseVisualStyleBackColor = false;
@@ -167,10 +160,9 @@
             BaoCaoDoanhThu_Button.BackColor = SystemColors.ActiveCaptionText;
             BaoCaoDoanhThu_Button.Dock = DockStyle.Fill;
             BaoCaoDoanhThu_Button.ForeColor = SystemColors.ButtonHighlight;
-            BaoCaoDoanhThu_Button.Location = new Point(1624, 7);
-            BaoCaoDoanhThu_Button.Margin = new Padding(7, 7, 7, 7);
+            BaoCaoDoanhThu_Button.Location = new Point(676, 3);
             BaoCaoDoanhThu_Button.Name = "BaoCaoDoanhThu_Button";
-            BaoCaoDoanhThu_Button.Size = new Size(255, 132);
+            BaoCaoDoanhThu_Button.Size = new Size(106, 56);
             BaoCaoDoanhThu_Button.TabIndex = 5;
             BaoCaoDoanhThu_Button.Text = "Lập báo cáo doanh thu";
             BaoCaoDoanhThu_Button.UseVisualStyleBackColor = false;
@@ -181,10 +173,9 @@
             ThayDoiQuyDinh_Button.BackColor = SystemColors.ActiveCaptionText;
             ThayDoiQuyDinh_Button.Dock = DockStyle.Fill;
             ThayDoiQuyDinh_Button.ForeColor = SystemColors.ButtonHighlight;
-            ThayDoiQuyDinh_Button.Location = new Point(1893, 7);
-            ThayDoiQuyDinh_Button.Margin = new Padding(7, 7, 7, 7);
+            ThayDoiQuyDinh_Button.Location = new Point(788, 3);
             ThayDoiQuyDinh_Button.Name = "ThayDoiQuyDinh_Button";
-            ThayDoiQuyDinh_Button.Size = new Size(255, 132);
+            ThayDoiQuyDinh_Button.Size = new Size(106, 56);
             ThayDoiQuyDinh_Button.TabIndex = 6;
             ThayDoiQuyDinh_Button.Text = "Thay đổi quy định";
             ThayDoiQuyDinh_Button.UseVisualStyleBackColor = false;
@@ -196,10 +187,9 @@
             BaoCaoPhieuNhap_Button.Dock = DockStyle.Fill;
             BaoCaoPhieuNhap_Button.Font = new Font("Arial", 9F);
             BaoCaoPhieuNhap_Button.ForeColor = SystemColors.ButtonHighlight;
-            BaoCaoPhieuNhap_Button.Location = new Point(2162, 7);
-            BaoCaoPhieuNhap_Button.Margin = new Padding(7, 7, 7, 7);
+            BaoCaoPhieuNhap_Button.Location = new Point(900, 3);
             BaoCaoPhieuNhap_Button.Name = "BaoCaoPhieuNhap_Button";
-            BaoCaoPhieuNhap_Button.Size = new Size(261, 132);
+            BaoCaoPhieuNhap_Button.Size = new Size(109, 56);
             BaoCaoPhieuNhap_Button.TabIndex = 8;
             BaoCaoPhieuNhap_Button.Text = " Báo cáo nhập VTPT";
             BaoCaoPhieuNhap_Button.UseVisualStyleBackColor = false;
@@ -215,12 +205,11 @@
             tableLayoutPanel2.Controls.Add(label3, 2, 0);
             tableLayoutPanel2.Controls.Add(pictureBox1, 3, 0);
             tableLayoutPanel2.Dock = DockStyle.Top;
-            tableLayoutPanel2.Location = new Point(0, 146);
-            tableLayoutPanel2.Margin = new Padding(7, 7, 7, 7);
+            tableLayoutPanel2.Location = new Point(0, 62);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.Size = new Size(2430, 129);
+            tableLayoutPanel2.Size = new Size(1012, 55);
             tableLayoutPanel2.TabIndex = 1;
             // 
             // label3
@@ -229,10 +218,9 @@
             label3.Dock = DockStyle.Fill;
             label3.Font = new Font("Arial", 11F);
             label3.ForeColor = SystemColors.Highlight;
-            label3.Location = new Point(1465, 0);
-            label3.Margin = new Padding(7, 0, 7, 0);
+            label3.Location = new Point(609, 0);
             label3.Name = "label3";
-            label3.Size = new Size(787, 129);
+            label3.Size = new Size(327, 55);
             label3.TabIndex = 4;
             label3.Text = "Trịnh Trần Phương Tuấn";
             label3.TextAlign = ContentAlignment.MiddleRight;
@@ -240,10 +228,9 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.avatar_icon_blue;
-            pictureBox1.Location = new Point(2266, 7);
-            pictureBox1.Margin = new Padding(7, 7, 7, 7);
+            pictureBox1.Location = new Point(942, 3);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(119, 115);
+            pictureBox1.Size = new Size(50, 49);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
@@ -252,23 +239,20 @@
             // 
             pictureBox2.Dock = DockStyle.Fill;
             pictureBox2.Image = Properties.Resources.manhinhchinh;
-            pictureBox2.Location = new Point(0, 275);
-            pictureBox2.Margin = new Padding(7, 7, 7, 7);
+            pictureBox2.Location = new Point(0, 117);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(2430, 783);
+            pictureBox2.Size = new Size(1012, 333);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
             // 
             // ManHinhChinh
             // 
-            AutoScaleDimensions = new SizeF(19F, 47F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2430, 1058);
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1012, 450);
             Controls.Add(pictureBox2);
             Controls.Add(tableLayoutPanel2);
             Controls.Add(tableLayoutPanel1);
-            Margin = new Padding(7, 7, 7, 7);
             Name = "ManHinhChinh";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "ManHinhChinh";
